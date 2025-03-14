ba311bdacc935d7ce7f75e4d8ea75dab3dffaac6
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2025-03-14 17:44:50 -0400

    create neondb table

---
+++ create_equipment_table.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE "gear_vault_schema"."equipment" (
+  "ID" integer PRIMARY KEY,
+  "NAME" text,
+  "BRAND" text,
+  "MODEL" text,
+  "STORE" text,
+  "SIZE" text,
+  "UNIT" integer,
+  "PURCHASE_DATE" date,
+  "NOTES" text
+)
